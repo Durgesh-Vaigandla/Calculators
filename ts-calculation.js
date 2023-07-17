01a90d0a6d02b74c79d8a029a36f8aa92c8d6400
Durgesh <vvip202014@gmail.com>
2023-07-17 19:38:08 +0530

    V2

---
+++ ts-calculation.js
@@ -0,0 +1,54 @@
+document.getElementById("marksCalc").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission from refreshing the page
+
+    function numberToWord(number) {
+        const words = ["ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"];
+        const numberString = number.toString();
+        const digits = numberString.split("");
+        const result = digits.map(digit => words[parseInt(digit)]);
+        return result.join(" ");
+    }
+
+    let firstSem = parseFloat(document.getElementById("1stSem").value);
+    let secondSem = parseFloat(document.getElementById("2ndSem").value);
+    let thirdSem = parseFloat(document.getElementById("3rdSem").value);
+    let fourthSem = parseFloat(document.getElementById("4thSem").value);
+    let fifthSem = parseFloat(document.getElementById("5thSem").value);
+    let sixthSem = parseFloat(document.getElementById("6thSem").value);
+
+    // Validate if all fields are filled with valid numbers
+    if (isNaN(firstSem) || isNaN(secondSem) || isNaN(thirdSem) || isNaN(fourthSem) || isNaN(fifthSem) || isNaN(sixthSem)) {
+        alert("Please fill in all fields with valid numbers before submitting.");
+        return;
+    }
+    let finalResult = firstSem + secondSem + thirdSem + fourthSem + fifthSem + sixthSem;
+    let maxMarks = 5300;
+    let Percentage = finalResult / maxMarks * 100;
+    //Marks Secured
+    document.getElementById("1stSemSecured").innerHTML = firstSem;
+    document.getElementById("2ndSemSecured").innerHTML = secondSem;
+    document.getElementById("3rdSemSecured").innerHTML = thirdSem;
+    document.getElementById("4thSemSecured").innerHTML = fourthSem;
+    document.getElementById("5thSemSecured").innerHTML = fifthSem;
+    document.getElementById("6thSemSecured").innerHTML = sixthSem;
+    //Marks Considered
+    document.getElementById("1stSemFigure").innerHTML = (firstSem + "(100%)");
+    document.getElementById("2ndSemFigure").innerHTML = (secondSem + "(100%)");
+    document.getElementById("3rdSemFigure").innerHTML = (thirdSem + "(100%)");
+    document.getElementById("4thSemFigure").innerHTML = (fourthSem + "(100%)");
+    document.getElementById("5thSemFigure").innerHTML = (fifthSem + "(100%)");
+    document.getElementById("6thSemFigure").innerHTML = (sixthSem + "(100%)");
+    //Marks in Words
+    document.getElementById("1stSemWord").innerHTML = numberToWord(firstSem);
+    document.getElementById("2ndSemWord").innerHTML = numberToWord(secondSem);
+    document.getElementById("3rdSemWord").innerHTML = numberToWord(thirdSem);
+    document.getElementById("4thSemWord").innerHTML = numberToWord(fourthSem);
+    document.getElementById("5thSemWord").innerHTML = numberToWord(fifthSem);
+    document.getElementById("6thSemWord").innerHTML = numberToWord(sixthSem);
+    //Total Marks
+    document.getElementById("totalWord").innerHTML = numberToWord(finalResult);
+    document.getElementById("totalMarks").innerHTML = finalResult;
+    document.getElementById("totalPercentage").innerHTML = (Percentage.toFixed(2) + "%");
+
+    document.getElementById("marksCalc").reset();
+});
